f2759e8663132a727c77673b3848517d4e57a29d
Limar <limarextraemail@gmail.com>
2023-10-31 19:40:39 +0800

    Delete pages/api directory

--- pages/api/bag.js
+++
@@ -1,8 +0,0 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import { Recipe } from "@/models/Recipe";
-
-export default async function handle(req, res) {
-    await mongooseConnect();
-    const ids = req.body.ids;
-    res.json(await Recipe.find({_id:ids}));
-}
